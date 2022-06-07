ef6a874e94f18ba2f1871583922c74bd271034ae
Emon <89279974+nh9686ay@users.noreply.github.com>
2022-06-06 19:20:56 -0500

    initializing backend

---
+++ backend/schemas/testimonials.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields:[
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'Imageurl',
+            type: 'image',
+            options:{
+                hotspot: true,
+            }
+        }
+    ]
+}
